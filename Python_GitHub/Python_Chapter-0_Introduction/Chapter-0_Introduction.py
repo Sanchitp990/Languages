d54d0a39215132c70efc2adf09a6eadbe33f8b86
Sanchit <135049449+Sanchitp990@users.noreply.github.com>
2023-12-24 20:05:26 +0530

    Python Chapter-0_Introduction

--- Python_GitHub/Python_Chapter-0_Introduction/Chapter-0_Introduction.py
+++ Python_GitHub/Python_Chapter-0_Introduction/Chapter-0_Introduction.py
@@ -7,4 +7,4 @@ print("Hello, world!")
 x = 5  # x is inferred to be an integer
 print(x) # output of x is 5
 x = "hello"  # now x is inferred to be a string
-print(x) # now the output of x is hello
+print(x) # now the output of x is hell
